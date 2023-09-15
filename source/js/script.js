4aaea4aba8a036b4d06d5836b866c319a4def912
Anton Pashkovskyi <anton@globalbit.co.il>
2023-09-15 14:05:44 +0200

    add smooth scroll

--- source/js/script.js
+++ source/js/script.js
@@ -8,25 +8,6 @@
     once: true
   });
 
-  
-  $(window).on('scroll', function () {
-		//.Scroll to top show/hide
-    var scrollToTop = $('.scroll-top-to'),
-      scroll = $(window).scrollTop();
-    if (scroll >= 200) {
-      scrollToTop.fadeIn(200);
-    } else {
-      scrollToTop.fadeOut(100);
-    }
-  });
-	// scroll-to-top
-  $('.scroll-top-to').on('click', function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 500);
-    return false;
-  });
-
   $(document).ready(function() {
 
     // navbarDropdown
@@ -151,16 +132,6 @@
         }
       ]
     });
-
-
-    // scroll
-    // $('.scrollTo').on('click', function (e) {
-    //   e.preventDefault();
-    //   var target = $(this).attr('href');
-    //   $('html, body').animate({
-    //     scrollTop: ($(target).offset().top)
-    //   }, 500);
-    // });
 
   });
 
